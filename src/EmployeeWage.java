1edc2ad1f9a300db75a241167f43b5009299cbec
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-18 18:19:32 +0530

    ADDED: UC-6

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -10,24 +10,31 @@ public class EmployeeWage {
     static final int empWagePerHour = 20;
     static final int empFullTime = 8;
     static final int empPartTime = 4;
-    static final int monthDays=20;
+    static final int monthDays = 20;
+    static final int maxWorkHours = 100;
 
 
-    public int getTotalMonthWage() {
+    public void getLimitMonthWage() {
         Random random = new Random();
         int totalWorkHrs = 0;
 
-        int i ;
-        for(i=1;i<=monthDays;i++) {
-            int workDoneHrs = random.nextInt((8) + 1);
-            totalWorkHrs = totalWorkHrs + workDoneHrs;
-        }
-        int monthWages= totalWorkHrs * empWagePerHour;
-        return monthWages;
+        int i;
 
+        for (i = 1; i <= monthDays; i++)    {
+            if(totalWorkHrs <= maxWorkHours) {
+                int workDoneHrs = random.nextInt((8) )+ 1;
+                System.out.println("Work done on day "+i+" is "+workDoneHrs);
+                totalWorkHrs = totalWorkHrs + workDoneHrs;
+                System.out.println("Total Work Hours till day "+i+" is "+totalWorkHrs);
+
+            }
+        }
+        int monthWages = totalWorkHrs * empWagePerHour;
+        System.out.println("Total wages: "+monthWages);
     }
+
     public static void main(String[] args) {
         EmployeeWage employeeWage = new EmployeeWage();
-        System.out.println("Total Wages of a month: "+employeeWage.getTotalMonthWage());
+        employeeWage.getLimitMonthWage();
     }
 }
