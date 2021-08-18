ec57a7a328eb9481fd96133ffaad47181e369216
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-18 16:58:18 +0530

    ADDED: UC-2

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,6 +1,15 @@
 import java.util.Random;
 
+/**
+ * @author Shubham
+ * @version 1.0
+ * @since 18-08-2021
+ */
+
 public class EmployeeWage {
+    static final int empWagePerHour = 20;
+    static final int empFulldayHour = 8;
+
     public void isPresent() {
         Random random = new Random();
         int attendance = random.nextInt(2);
@@ -10,8 +19,13 @@ public class EmployeeWage {
             System.out.println("Employee ABSENT");
     }
 
+    public int giveTotalWage() {
+        return empFulldayHour * empWagePerHour;
+
+    }
+
     public static void main(String[] args) {
         EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.isPresent();
+        System.out.println("Total Wages :" + employeeWage.giveTotalWage());
     }
 }
