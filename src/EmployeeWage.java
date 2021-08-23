80f8a1c1df10e619283771e9866c13bd4e79ac8e
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-24 00:36:33 +0530

    ADDED: UC-9

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,4 +1,3 @@
-
 /**
  * @author Shubham
  * @version 1.0
@@ -8,28 +7,34 @@
 public class EmployeeWage {
     public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
-    public int EMP_RATE_PER_HOUR;
-    public int NUM_OF_WORKING_DAYS;
-    public int MAX_HRS_IN_A_MONTH;
-    public String COMPANY_NAME ;
+    public int empRatePerHour;
+    public int maxWorkingDaysPerMonth;
+    public int maxHoursPerMonth;
+    public String companyName;
+    public int totalEmployeeWage;
 
-    public EmployeeWage( String COMPANY_NAME,int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_A_MONTH) {
-        this.COMPANY_NAME=COMPANY_NAME;
-        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-        this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
-        this.MAX_HRS_IN_A_MONTH = MAX_HRS_IN_A_MONTH;
+    /**
+     * @param empRatePerHour : Amount received per hour
+     * @param maxHoursPerMonth : The limit of amount a employee can work per month
+     * @param maxWorkingDaysPerMonth : The number of days a employee can work
+     */
+
+
+    public EmployeeWage(String companyName, int maxHoursPerMonth,int maxWorkingDaysPerMonth,int empRatePerHour ) {
+        this.companyName=companyName;
+        this.empRatePerHour=empRatePerHour;
+        this.maxWorkingDaysPerMonth=maxWorkingDaysPerMonth;
+        this.maxHoursPerMonth=maxHoursPerMonth;
+
+
     }
 
-//Method
-    public  int computeEmpWage() {
-        System.out.println("--------------------------------------------");
-        System.out.println("Employee Wage Computation for "+COMPANY_NAME);
-        System.out.println("--------------------------------------------");
-        int empHrs = 0;
+    public void computeEmpWage() {
+        int empHrs;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
-        while (totalEmpHrs < MAX_HRS_IN_A_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < maxWorkingDaysPerMonth) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -43,24 +48,37 @@ public class EmployeeWage {
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
-            System.out.println("DAY: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+
         }
-        System.out.println("Total work hours: "+totalEmpHrs);
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
-        return totalEmpWage;
+
+        totalEmployeeWage = totalEmpHrs * empRatePerHour;
+
+    }
+    @Override
+    public String toString(){
+        return "Total Employee Wage for Company: "+companyName+" is "+totalEmployeeWage;
     }
 
     public static void main(String[] args) {
-        //COMPANY 1
-        EmployeeWage crisil=new EmployeeWage("Crisil",25,20,100);
-        System.out.println(crisil.computeEmpWage());
-        //COMPANY 2
-        EmployeeWage tcs =new EmployeeWage("TCS",50,20,100);
-        System.out.println(tcs.computeEmpWage());
 
+        //uc-9
+        //COMPANY 1
+
+        EmployeeWage crisil=new EmployeeWage("CRSISIL",200,20,30);
+        crisil.computeEmpWage();
+        System.out.println(crisil);
+        //COMPANY 2
+        EmployeeWage tcs =new EmployeeWage("TCS",200,23,28);
+        tcs.computeEmpWage();
+        tcs.computeEmpWage();
+        System.out.println(tcs);
+
+        }
     }
 
-}
+
+
+
+
 
 
